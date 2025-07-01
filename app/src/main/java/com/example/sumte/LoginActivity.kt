2791337068b1548a0465374a05675b6dbe3fe721
Jhyun02 <hjh75035117@gmail.com>
2025-07-01 16:29:15 +0900

    feat: login 화면 구현

---
+++ app/src/main/java/com/example/sumte/LoginActivity.kt
@@ -0,0 +1,39 @@
+package com.example.sumte
+
+import android.app.Activity
+import android.os.Bundle
+import android.text.InputType
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sumte.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private var isPasswordVisible = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivTogglePassword.setOnClickListener {
+            isPasswordVisible = !isPasswordVisible
+
+            if (isPasswordVisible) {
+                binding.etPassword.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                binding.ivTogglePassword.setImageResource(R.drawable.show)
+            } else {
+                binding.etPassword.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                binding.ivTogglePassword.setImageResource(R.drawable.hide)
+            }
+
+            binding.etPassword.setSelection(binding.etPassword.text.length)
+        }
+
+    }
+}
