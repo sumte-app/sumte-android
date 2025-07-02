2afac2995f9cb4f77a566e4ca363bd907d509891
이경민 <igyeongmin@igyeongmin-ui-noteubug.local>
2025-07-02 12:21:16 +0900

    필터링 부분 레이아웃 디자인

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
