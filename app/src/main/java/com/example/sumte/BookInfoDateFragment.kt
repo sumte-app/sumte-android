46c7ed8a34d831fd66e382ef37c4125d3fc573a3
ChaeLeeee <cwlee.red25@gmail.com>
2025-07-05 02:36:02 +0900

    only selected style issue left

--- app/src/main/java/com/example/sumte/BookInfoDateFragment.kt
+++ app/src/main/java/com/example/sumte/BookInfoDateFragment.kt
@@ -20,8 +20,8 @@ import java.time.YearMonth
 class BookInfoDateFragment : Fragment() {
     lateinit var binding: FragmentBookInfoDateBinding
 
-    private var startDate: LocalDate? = null
-    private var endDate: LocalDate? = null
+    private var startDate: LocalDate? = LocalDate.now()
+    private var endDate: LocalDate? = LocalDate.now().plusDays(1)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,7 +37,17 @@ class BookInfoDateFragment : Fragment() {
             val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
         }
 
-
+        fun getKoreanDayOfWeek(date: LocalDate): String {
+            return when (date.dayOfWeek) {
+                java.time.DayOfWeek.MONDAY -> "월"
+                java.time.DayOfWeek.TUESDAY -> "화"
+                java.time.DayOfWeek.WEDNESDAY -> "수"
+                java.time.DayOfWeek.THURSDAY -> "목"
+                java.time.DayOfWeek.FRIDAY -> "금"
+                java.time.DayOfWeek.SATURDAY -> "토"
+                java.time.DayOfWeek.SUNDAY -> "일"
+            }
+        }
 
         binding.customCalendar.dayBinder = object : MonthDayBinder<DayViewContainer>{
             override fun create(view: View) = DayViewContainer(view)
@@ -52,20 +62,6 @@ class BookInfoDateFragment : Fragment() {
                 container.textView.alpha = 1f
                 container.textView.isClickable = true
 
-                //일요일. 공휴일은 api도입 고려중
-                if (date.dayOfWeek == java.time.DayOfWeek.SUNDAY) {
-                    container.textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.red))
-                }
-                //오늘 이전
-                if (date.isBefore(LocalDate.now())) {
-                    container.textView.alpha = 0.3f
-                    container.textView.isClickable = false
-                    return
-                }
-                //오늘
-                if (date == LocalDate.now()) {
-                    container.textView.setBackgroundResource(R.drawable.today_circle)
-                }
                 //선택 가능한 때 정의
                 val selected = when {
                     startDate != null && endDate != null ->
@@ -73,9 +69,8 @@ class BookInfoDateFragment : Fragment() {
                     startDate != null -> date == startDate
                     else -> false
                 }
-
                 if (selected) {
-                    if (startDate != null && endDate != null) {
+                    if (startDate != null && endDate != null) {//범위선택
                         when (date) {
                             startDate -> container.textView.setBackgroundResource(R.drawable.selcted_circle_start)
                             endDate -> container.textView.setBackgroundResource(R.drawable.selcted_circle_end)
@@ -83,7 +78,6 @@ class BookInfoDateFragment : Fragment() {
                             else -> container.textView.background = null
                         }
                     } else if (startDate != null) {
-                        // startDate만 있을 때는 단일 선택이므로 전체 둥근 원형 배경
                         if (date == startDate) {
                             container.textView.setBackgroundResource(R.drawable.selected_circle)
                         } else {
@@ -96,9 +90,33 @@ class BookInfoDateFragment : Fragment() {
                     container.textView.background = null
                 }
 
+                //일요일. 공휴일은 api도입 고려중
+                if (date.dayOfWeek == java.time.DayOfWeek.SUNDAY) {
+                    container.textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.red))
+                }
+                //오늘 이전
+                if (date.isBefore(LocalDate.now())) {
+                    container.textView.alpha = 0.3f
+                    container.textView.isClickable = false
+                    return
+                }
+                //오늘
+                if (date == LocalDate.now()) {
+                    val isNotSelected =
+                        !(startDate != null && date == startDate) &&
+                                !(endDate != null && date == endDate) &&
+                                !(startDate != null && endDate != null && date in (startDate!!..endDate!!))
+                    if (isNotSelected) {
+                        container.textView.setBackgroundResource(R.drawable.today_circle)
+                    }
+                }
 
                 container.textView.setOnClickListener {
                     val clickedDate = date
+                    if (startDate != null && endDate == null && clickedDate == startDate) {
+                        return@setOnClickListener
+                    }
+
                     when {
                         startDate == null || endDate != null -> {
                             startDate = clickedDate
@@ -112,10 +130,24 @@ class BookInfoDateFragment : Fragment() {
                         }
                     }
 
+                    startDate?.let {
+                        val startDayKor = getKoreanDayOfWeek(it)
+                        binding.startDate.text = String.format("%d.%02d %s", it.monthValue, it.dayOfMonth, startDayKor)
+                    }
+                    if (endDate != null) {
+                        endDate?.let {
+                            val endDayKor = getKoreanDayOfWeek(it) 
+                            binding.endDate.text = String.format("%d.%02d %s", it.monthValue, it.dayOfMonth, endDayKor)
+                        }
+                        val nights = java.time.temporal.ChronoUnit.DAYS.between(startDate, endDate)
+                        binding.dateCount.text = "${nights}박"
+                    } else {
+                        binding.endDate.text = ""
+                        binding.dateCount.text = ""
+                    }
                     binding.customCalendar.notifyCalendarChanged()
                 }
             }
-
         }
         binding.customCalendar.apply {
             val currentMonth = YearMonth.now()
@@ -132,8 +164,16 @@ class BookInfoDateFragment : Fragment() {
                 binding.todayMonthText.text = String.format("%d.%02d", year, month)
             }
         }
-
+        
+        //추후보수
+        val currentDay = LocalDate.now()
         val currentMonth = YearMonth.now()
+        val dayOfWeekKor = getKoreanDayOfWeek(currentDay)
+        val endPlusOne = currentDay.plusDays(1)
+        val endDayOfWeekKor = getKoreanDayOfWeek(endPlusOne)
+
+        binding.startDate.text = String.format("%d.%02d %s", currentDay.monthValue, currentDay.dayOfMonth, dayOfWeekKor)
+        binding.endDate.text = String.format("%d.%02d %s,", endPlusOne.monthValue, endPlusOne.dayOfMonth, endDayOfWeekKor)
         binding.todayMonthText.text = String.format("%d.%02d", currentMonth.year, currentMonth.monthValue)
     }
 }
