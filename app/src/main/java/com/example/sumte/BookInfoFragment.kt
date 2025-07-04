f4e826bb3331f9121cfd1de5235450945706b3e5
ChaeLeeee <cwlee.red25@gmail.com>
2025-07-04 15:43:39 +0900

    calendar color

--- app/src/main/java/com/example/sumte/BookInfoFragment.kt
+++ app/src/main/java/com/example/sumte/BookInfoFragment.kt
@@ -1,10 +1,11 @@
 package com.example.sumte
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.sumte.databinding.CalendarDayLayoutBinding
 import com.example.sumte.databinding.FragmentBookInfoBinding
@@ -13,16 +14,11 @@ import com.kizitonwose.calendar.core.daysOfWeek
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
 import com.kizitonwose.calendar.view.MonthDayBinder
 import com.kizitonwose.calendar.view.ViewContainer
-import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
 
 class BookInfoFragment : Fragment() {
     lateinit var binding: FragmentBookInfoBinding
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//    }
 
     private var startDate: LocalDate? = null
     private var endDate: LocalDate? = null
@@ -43,24 +39,46 @@ class BookInfoFragment : Fragment() {
 
         binding.customCalendar.dayBinder = object : MonthDayBinder<DayViewContainer>{
             override fun create(view: View) = DayViewContainer(view)
-            // 컨테이너가 재사용될 때마다 호출
 
             override fun bind(container: DayViewContainer, data: CalendarDay) {
-                // DayViewContainer의 day 변수에 data 할당
-                //container.day = data
-                container.textView.text = data.date.dayOfMonth.toString()
+                val date = data.date
+                container.textView.text = date.dayOfMonth.toString()
+                container.textView.setTypeface(null, Typeface.BOLD)
+
+                //선택불가
+                if (date.isBefore(LocalDate.now())) {
+                    container.textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray400)) // 흐린색
+                    container.textView.alpha = 0.5f
+                    container.textView.isClickable = false
+                    return
+                }//오늘
+                if (date == LocalDate.now()) {
+                    container.textView.setBackgroundResource(R.drawable.today_circle)
+                    container.textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+                }
+                //공휴일은 추후 외부 api연동 고민좀
+                val isSunday = date.dayOfWeek == java.time.DayOfWeek.SUNDAY
+                if (isSunday) {
+                    container.textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.red)) // 빨간색
+                }
+                else {
+                    container.textView.setBackgroundResource(0) // 배경 제거
+                    container.textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+                }
+
             }
         }
         binding.customCalendar.apply {
             val currentMonth = YearMonth.now()
-            val firstMonth = currentMonth.minusMonths(240)
+            val firstMonth = currentMonth
             val lastMonth = currentMonth.plusMonths(240)
             val firstDayOfWeek = firstDayOfWeekFromLocale()
 
             setup(firstMonth, lastMonth, firstDayOfWeek)
             scrollToMonth(currentMonth)
+
         }
-        val daysOfWeek = daysOfWeek()
+
 
     }
 
