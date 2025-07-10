ff6eacd69aff7e6ab922491936b58bf9551763c1
GyeongminLee <kmin817@naver.com>
2025-07-10 14:56:38 +0900

    후기 작성 레이아웃 완료 + 기능 구현 완료(후기 등록 제외)

---
+++ app/src/main/java/com/example/sumte/ReviewPhotoAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.sumte
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sumte.databinding.ItemReviewPhotoBinding
+
+class ReviewPhotoAdapter(
+    private val photos: MutableList<Uri>,
+    private val onRemoveClick: (Int) -> Unit
+) : RecyclerView.Adapter<ReviewPhotoAdapter.PhotoViewHolder>() {
+
+    inner class PhotoViewHolder(private val binding: ItemReviewPhotoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(uri: Uri) {
+            binding.photoImageView.setImageURI(uri)
+
+            binding.removeButton.setOnClickListener {
+                onRemoveClick(adapterPosition)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val binding = ItemReviewPhotoBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return PhotoViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = photos.size
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        holder.bind(photos[position])
+    }
+}
---
+++ app/src/main/java/com/example/sumte/ReviewWriteActivity.kt
@@ -0,0 +1,148 @@
+package com.example.sumte
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sumte.databinding.ActivityReviewWriteBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.io.File
+
+class ReviewWriteActivity:AppCompatActivity() {
+    lateinit var binding: ActivityReviewWriteBinding
+    private lateinit var cameraLauncher: ActivityResultLauncher<Uri>
+    private lateinit var galleryLauncher: ActivityResultLauncher<String>
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
+    private val photoList = mutableListOf<Uri>()
+    private lateinit var reviewPhotoAdapter: ReviewPhotoAdapter
+
+    private var cameraImageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityReviewWriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val granted = permissions.all { it.value }
+            if (!granted) {
+                Toast.makeText(this, "권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // 리싸이클러 및 어댑터 초기화
+        reviewPhotoAdapter = ReviewPhotoAdapter(photoList) { position ->
+            photoList.removeAt(position)
+            reviewPhotoAdapter.notifyItemRemoved(position)
+
+            if (photoList.isEmpty()) {
+                binding.reviewPicShowLl.visibility = View.GONE
+            }
+        }
+
+        binding.reviewPicShowRv.apply {
+            adapter = reviewPhotoAdapter
+            layoutManager = LinearLayoutManager(this@ReviewWriteActivity, RecyclerView.HORIZONTAL, false)
+        }
+
+        // 카메라 런처
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            if (success && cameraImageUri != null) {
+                photoList.add(cameraImageUri!!)
+                reviewPhotoAdapter.notifyItemInserted(photoList.lastIndex)
+                binding.reviewPicShowLl.visibility = View.VISIBLE
+            }
+        }
+
+        // 갤러리 런처
+        galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            uri?.let {
+                photoList.add(it)
+                reviewPhotoAdapter.notifyItemInserted(photoList.lastIndex)
+                binding.reviewPicShowLl.visibility = View.VISIBLE
+            }
+        }
+
+        // 바텀시트- 사진 추가 버튼 클릭시
+        binding.reviewPicAddLl.setOnClickListener {
+            val bottomSheet = PhotoOptionsBottomSheet()
+            bottomSheet.setOnOptionSelectedListener(object:PhotoOptionsBottomSheet.OnOptionSelectedListener{
+                override fun onTakePhotoSelected() {
+                    requestPermissionsIfNeeded {
+                        launchCamera()
+                    }
+                }
+
+                override fun onSelectFromAlbumSelected() {
+                    requestPermissionsIfNeeded {
+                        launchGallery()
+                    }
+                }
+            })
+            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+        }
+//        cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+//            if (success && cameraImageUri != null) {
+//                photoList.add(cameraImageUri!!)
+//                reviewPhotoAdapter.notifyItemInserted(photoList.lastIndex)
+//                binding.reviewPicShowLl.visibility = View.VISIBLE
+//            }
+//        }
+//
+//        galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+//            uri?.let {
+//                photoList.add(it)
+//                reviewPhotoAdapter.notifyItemInserted(photoList.lastIndex)
+//                binding.reviewPicShowLl.visibility = View.VISIBLE
+//            }
+//        }
+
+    }
+
+    private fun requestPermissionsIfNeeded(onGranted: () -> Unit) {
+        val permissions = mutableListOf<String>()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permissions.add(android.Manifest.permission.READ_MEDIA_IMAGES)
+        } else {
+            permissions.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        permissions.add(android.Manifest.permission.CAMERA)
+
+        val notGranted = permissions.filter {
+            ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }
+
+        if (notGranted.isEmpty()) {
+            onGranted()
+        } else {
+            permissionLauncher.launch(notGranted.toTypedArray())
+        }
+    }
+
+    private fun launchCamera() {
+        val photoFile = File.createTempFile("camera_photo_", ".jpg", cacheDir)
+        cameraImageUri = FileProvider.getUriForFile(
+            this,
+            "${packageName}.fileprovider",
+            photoFile
+        )
+        cameraLauncher.launch(cameraImageUri)
+    }
+
+    private fun launchGallery() {
+        galleryLauncher.launch("image/*")
+    }
+}
