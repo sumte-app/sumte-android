0047bd7ccddbb083a8bcdc81a99646ee515f5a71
GyeongminLee <kmin817@naver.com>
2025-07-14 16:39:20 +0900

    ReviewManage/ReviewDeleteAskDialog added(confrim func not yet)

---
+++ app/src/main/java/com/example/sumte/ReviewDeleteAskDialog.kt
@@ -0,0 +1,34 @@
+package com.example.sumte
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.example.sumte.databinding.DialogReviewDeleteAskBinding
+
+
+class ReviewDeleteAskDialog ( private val onConfirm: () -> Unit) : DialogFragment(){
+    private var binding: DialogReviewDeleteAskBinding?=null
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = Dialog(requireContext())
+        binding = DialogReviewDeleteAskBinding.inflate(layoutInflater)
+        dialog.setContentView(binding!!.root)
+
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        binding!!.reviewDeleteCancelTv.setOnClickListener {
+            onConfirm()
+            dismiss()
+        }
+        binding!!.reviewDeleteConfirmTv.setOnClickListener {
+            // 삭제 기능 구현 필요
+        }
+        return dialog
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding=null
+    }
+}
