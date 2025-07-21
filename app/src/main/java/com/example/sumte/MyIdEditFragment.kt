4cc8d032740f33524687069a14801f841ad9be2e
ChaeLeeee <cwlee.red25@gmail.com>
2025-07-21 22:38:41 +0900

    id error sign

--- app/src/main/java/com/example/sumte/MyIdEditFragment.kt
+++ app/src/main/java/com/example/sumte/MyIdEditFragment.kt
@@ -1,24 +1,61 @@
 package com.example.sumte
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.replace
 import com.example.sumte.databinding.FragmentMyIdEditBinding
 
-class MyIdEditFragment : Fragment(){
-    lateinit var binding: FragmentMyIdEditBinding
+class MyIdEditFragment : Fragment() {
+
+    private var _binding: FragmentMyIdEditBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
-        binding=FragmentMyIdEditBinding.inflate(inflater, container, false)
+        _binding = FragmentMyIdEditBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.editNickname.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val isEmpty = s.isNullOrEmpty()
+                showError(isEmpty)
+            }
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+    }
+
+    fun showError(isError: Boolean) {
+        if (isError) {
+            binding.editNickname.backgroundTintList = ColorStateList.valueOf(
+                ContextCompat.getColor(requireContext(), R.color.red)
+            )
+            binding.errorSign.visibility = View.VISIBLE
+        } else {
+            binding.editNickname.backgroundTintList = ColorStateList.valueOf(
+                ContextCompat.getColor(requireContext(), R.color.black)
+            )
+            binding.errorSign.visibility = View.GONE
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
