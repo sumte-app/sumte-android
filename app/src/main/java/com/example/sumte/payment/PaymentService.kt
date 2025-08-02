b0f34ad2d5e6bff7f60f832a28d17c9e4aae1c84
JunHyun <hjh75035117@gmail.com>
2025-08-02 16:57:06 +0900

    feat: PaymentService 추가

---
+++ app/src/main/java/com/example/sumte/payment/PaymentService.kt
@@ -0,0 +1,13 @@
+package com.example.sumte.payment
+
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface PaymentService {
+    @POST("/payments/request")
+    suspend fun requestPayment(
+        @Body body: PaymentRequest
+    ): Response<PaymentResponse>
+}
