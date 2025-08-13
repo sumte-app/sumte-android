1531dd3cc465dc3a1f373ab6b9065ad9d0fe455c
ChaeLeeee <cwlee.red25@gmail.com>
2025-08-13 22:52:29 +0900

    @post reservation

--- app/src/main/java/com/example/sumte/RetrofitClient.kt
+++ app/src/main/java/com/example/sumte/RetrofitClient.kt
@@ -1,18 +1,44 @@
 package com.example.sumte
 
+import android.util.Log
 import com.example.sumte.guesthouse.GuesthouseApi
 import com.example.sumte.roomregister.RoomRegisterService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.getValue
 import com.example.sumte.housedetail.RoomService
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.OkHttpClient
+
+
 
 object RetrofitClient {
+    //원래 private
     private const val BASE_URL = "https://sumteapi.duckdns.org/"
+
+    //디버깅용
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private fun createOkHttpClient(jwtToken: String?): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .addInterceptor { chain ->
+                val builder = chain.request().newBuilder()
+                jwtToken?.let {
+                    builder.addHeader("Authorization", it) // 토큰 그대로 넣기 (Bearer 포함)
+                }
+                chain.proceed(builder.build())
+            }
+            .build()
+    }
+
 
     val instance: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(OkHttpClient.Builder().addInterceptor(logging).build())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -32,6 +58,27 @@ object RetrofitClient {
 
     val api: GuesthouseApi by lazy {
         instance.create(GuesthouseApi::class.java)
+    }
+
+    //예약 api
+
+    fun createReservationService(token: String): ReservationService {
+        val client = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("Authorization", "Bearer $token")
+                    .build()
+                chain.proceed(request)
+            }
+            .build()
+
+        val retrofitWithToken = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofitWithToken.create(ReservationService::class.java)
     }
 
 }
